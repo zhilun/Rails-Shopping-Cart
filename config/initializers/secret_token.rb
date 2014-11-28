406c527577e4482049bcec86ea144aa4b8cefd55
zhilun <trankynam2612@gmail.com>
2014-11-28 17:27:03 +0700

    Depot application. End of task C. Catalog Display (P.104)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '7f18cc15019025f5b03fce0149cdf1961e3069eeaa9f47b4cbdb0d4744786f1593e24c5f003236f7dd02f5990f797c0f0bcac9b3979dc31a2b11a5d078ca2ee6'
