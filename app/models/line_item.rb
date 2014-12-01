c0ebea6a7c908045814898e2612152622ead4cec
zhilun <trankynam2612@gmail.com>
2014-12-01 17:35:03 +0700

    Iteration E3: Finising the cart Page 128

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
-  belongs_to :product
-  belongs_to :cart
+	attr_accessible :cart_id, :product_id
+	belongs_to :product
+	belongs_to :cart
+
+	def total_price
+		product.price * quantity
+	end
 end
