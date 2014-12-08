7fb02fa5356eb504f06d4f8c6db30e5ed609a189
zhilun <trankynam2612@gmail.com>
2014-12-08 14:18:36 +0700

    End of Task I: Logging In Page 208

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@
 #---
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authorize
 
   private
 
@@ -17,6 +18,14 @@ class ApplicationController < ActionController::Base
       cart = Cart.create
       session[:cart_id] = cart.id
       cart
+    end
+
+    protected
+
+    def authorize
+    	unless User.find_by_id(session[:user_id])
+    		redirect_to login_url, notice: "Please log in"
+    	end
     end
 end
 
